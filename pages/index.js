273f477378e4827df5e032992c5de954ac35348d
jbastias <jorgebastias@hotmail.com>
2017-09-04 14:46:44 -0600

    step 3

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,7 @@ export default () => (
   <div>
     <h1>Hello World.</h1>
     <Link href='/about'><a>About</a></Link>
+    <br />
+    <Link href='/slidebean'><a>Slidebean</a></Link>
   </div>
 )
--- pages/slidebean.js
+++ pages/slidebean.js
@@ -1,9 +1,9 @@
 import Link from 'next/link'
 export default () => (
   <div>
-    <h1>About us</h1>
+    <h1>Slidebean</h1>
     <Link href='/'><a>Home</a></Link>
     <br />
-    <Link href='/slidebean'><a>Slidebean</a></Link>
+    <Link href='/about'><a>About</a></Link>
   </div>
 )
